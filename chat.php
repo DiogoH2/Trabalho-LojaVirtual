c97203d9e7f55300c7f0fd054c9e787fbf0795d4
DiogoH2 <48698251+DiogoH2@users.noreply.github.com>
2020-06-25 02:02:38 -0300

    Add files via upload

--- chat.php
+++ chat.php
@@ -1,15 +1,3 @@
-<?php
-    session_start();
-    if(isset($_SESSION['usuario']) && is_array($_SESSION['usuario'])){
-        $adm = $_SESSION['usuario'][1];
-        $cliente = $_SESSION['usuario'][0];
-
-
-    }else{
-
-        echo "<script>window.location = 'index.php'</script>";
-    }
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
--- produtos.php
+++ produtos.php
@@ -1,3 +1,15 @@
+<?php
+    session_start();
+    if(isset($_SESSION['usuario']) && is_array($_SESSION['usuario'])){
+        $adm = $_SESSION['usuario'][1];
+        $cliente = $_SESSION['usuario'][0];
+
+
+    }else{
+
+        echo "<script>window.location = 'index.php'</script>";
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
